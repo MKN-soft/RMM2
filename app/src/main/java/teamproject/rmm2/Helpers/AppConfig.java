5342c7be8086d1ae2feb889da9db208cc277886f
Hippo14 <hippo14@gmail.com>
2015-09-08 00:19:10 +0200

    added SSL

--- app/src/main/java/teamproject/rmm2/Helpers/AppConfig.java
+++ app/src/main/java/teamproject/rmm2/Helpers/AppConfig.java
@@ -5,7 +5,8 @@ Class for Application settings.
  */
 public class AppConfig {
     // Server user API URL
-    public static final String URL_API = "http://lubiekokosy.pl/php/android.php";
+    //public static final String URL_API = "http://lubiekokosy.pl/php/android.php";
+    public static final String URL_API = "https://serwer1538500.home.pl/lubiekokosy/php/android.php";
 
     public static final String TAG_LOGIN = "login";
     public static final String TAG_REGISTER = "register";
